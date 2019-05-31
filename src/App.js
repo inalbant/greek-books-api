5e5b3b8850a323b323b0d7b178730f840f8c0fdc
Ibrahim Nalbant <i.nalbant90@gmail.com>
2019-05-31 13:00:26 +0100

    removed unnecessary row col

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import BookCovers from './components/BookCovers';
 
 class App extends Component {
@@ -20,11 +18,7 @@ class App extends Component {
       return (
         <div className="App">
           <Container>
-            <Row className="h-50" >
-              <Col className="h-100 d-table" >
-                <BookCovers books={this.state.books} className="d-table-cell align-middle" />
-              </Col>
-            </Row>
+            <BookCovers books={this.state.books} />
           </Container>
         </div>
       )
